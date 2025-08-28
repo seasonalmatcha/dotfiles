c40e3e49640832bd4e0ffe43432710255e86550b
Alan H <alan@peoplyee.com>
2025-08-28 17:44:12 +0700

    feat(nvim): oil config
    add list of files/dir to hide
    toggle preview
    toggle detail

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,27 @@
+local show_detail = false
+
+local to_hide = {
+  "node_modules",
+  "dist",
+  "coverage",
+  "build",
+}
+
 return {
   "stevearc/oil.nvim",
   config = function()
     local map = require("utils").map
     local oil = require("oil")
+
+    local function toggle_detail()
+      show_detail = not show_detail
+
+      if show_detail then
+        oil.set_columns({ "icon", "permissions", "size", "mtime" })
+      else
+        oil.set_columns({ "icon" })
+      end
+    end
 
     oil.setup({
       keymaps = {
@@ -12,11 +31,27 @@ return {
         ["<leader>h"] = { "actions.select", opts = { horizontal = true } },
         ["<backspace>"] = { "actions.parent" },
         ["."] = { "actions.toggle_hidden" },
+        ["gd"] = toggle_detail,
+        ["<C-d>"] = { "actions.preview_scroll_down" },
+        ["<C-u>"] = { "actions.preview_scroll_up" },
       },
       float = {
         padding = 2,
         max_width = 80,
         border = "rounded",
+      },
+      view_options = {
+        is_hidden_file = function(name, _)
+          if name:sub(1, 1) == "." then
+            return true
+          end
+          for _, pattern in ipairs(to_hide) do
+            if name == pattern then
+              return true
+            end
+          end
+          return false
+        end,
       },
     })
 
